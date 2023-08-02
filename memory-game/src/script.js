c50c8c158f50b2e698581006e412185b7296c53c
arthur <arthurrodrigues.contato@outlook.com>
2023-08-02 20:24:14 -0300

    memory game finalized

--- memory-game/src/script.js
+++ memory-game/src/script.js
@@ -1,14 +1,45 @@
 const _cards = document.querySelectorAll('.card');
+const _restart = document.querySelector('.restart')
+let cardsFliped = [];
 
-console.log(_cards);
+document.addEventListener('DOMContentLoaded', () => {
+    shuffle();
+    eventListeners();
+})
 
-(function shuffle() {
+function eventListeners() {
+    _restart.addEventListener('click', () => restartGame());
+    
+    _cards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('active');
+            cardsFliped.push(card);
+    
+            if (cardsFliped.length === 2) {
+                verify(cardsFliped);
+                cardsFliped = [];
+            }
+        })
+    })
+}
+
+function shuffle() {
     _cards.forEach(card => {
         const randomIndex = Math.floor(Math.random() * 16);
         card.style.order = randomIndex;
     });
-})();
+};
 
-_cards.forEach(card => {
-    card.addEventListener('click', () => card.classList.toggle('active'));
-})
+function verify(cardsFliped) {
+    if (cardsFliped[0].children[0].src !== cardsFliped[1].children[0].src) {
+        setTimeout(() => {
+            cardsFliped.forEach(card => card.classList.remove('active'));
+        }, 300)
+
+    } 
+}
+
+function restartGame() {
+    _cards.forEach(card => card.classList.remove('active'));
+    shuffle();
+}
