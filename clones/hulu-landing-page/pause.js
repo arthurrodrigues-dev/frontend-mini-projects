e18d36b9989bf72efa633928f692d034d542ef00
arthur <arthurrodrigues.contato@outlook.com>
2023-08-12 12:07:43 -0300

    pause btn script

--- clones/hulu-landing-page/pause.js
+++ clones/hulu-landing-page/pause.js
@@ -1,11 +1,11 @@
 const pauseBtn = document.querySelector('.pause-btn');
 const video = document.querySelector('video');
 
-pauseBtn.addEventListener('click', (e) => {
+pauseBtn.addEventListener('click', () => {
     const icon = pauseBtn.querySelector('i')
     icon.classList.toggle('bx-pause');
     icon.classList.toggle('bxs-right-arrow');
-    if (e.target.classList[1] === 'bxs-right-arrow') {
+    if (icon.classList[1] === 'bxs-right-arrow') {
         video.pause();
         return;
     }
