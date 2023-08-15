a53c29ebdcc80897540652496fd112f0239c55d0
arthur <arthurrodrigues.contato@outlook.com>
2023-08-14 22:03:09 -0300

    change spotlight backgrounds

--- clones/hulu-landing-page/js/spotlight.js
+++ clones/hulu-landing-page/js/spotlight.js
@@ -3,6 +3,7 @@ const spotlightChildrens = Array.from(spotlightContent.children)
 const spotlightDivsContent = spotlightChildrens.filter(children => children.classList[0] !== 'btns')
 const divButtons = spotlightContent.querySelectorAll('.btn');
 const buttons = spotlightContent.querySelectorAll('button');
+const spotlightBackground = document.querySelector('.spotlight-background') 
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -13,20 +14,25 @@ buttons.forEach(button => {
         spotlightDivsContent
             .filter(div => div.classList[0] !== buttonId)
             .forEach(div => {
-                if (!div.classList.contains('inactive')) div.classList.add('inactive')
+                if (!div.classList.contains('inactive')) addClass(div, 'inactive')
             })
         
-        spotlightDivsContent.filter(div => div.classList[0] === buttonId)[0].classList.remove('inactive');
+        const activeDiv = spotlightDivsContent.filter(div => div.classList[0] === buttonId)[0];
+        removeClass(activeDiv, 'inactive');
+        changeBackground(activeDiv);
     })
 })
 
+function removeClass(element, classe) {
+    element.classList.remove(classe);
+}
 
 function removeClasses(array, classe) {
-    array.forEach(element => element.classList.remove(classe))
+    array.forEach(element => element.classList.remove(classe));
 }
 
 function addClasses(array, classe) {
-    array.forEach(element => element.classList.add(classe))
+    array.forEach(element => element.classList.add(classe));
 }
 
 function addClass(element, classe) {
@@ -36,4 +42,13 @@ function addClass(element, classe) {
 function styleButton(button) {
     removeClasses(divButtons, 'btn-active');
     addClass(button, 'btn-active');
+}
+
+function changeBackground(div) {
+    const divId = div.classList[0];
+    const path = `assets/images/backgrounds/${divId}.jpg`
+        
+    const img = spotlightBackground.querySelector('img');
+    img.setAttribute('src', path);
+    
 }
