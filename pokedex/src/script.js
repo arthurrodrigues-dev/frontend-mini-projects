b7fb418e3d219cd50fca197a3231b4cab633fab7
arthur <arthurrodrigues.contato@outlook.com>
2023-09-06 20:56:41 -0300

    creating cards dinamically

--- pokedex/src/script.js
+++ pokedex/src/script.js
@@ -1,12 +1,11 @@
-const _cards = Array.from(document.querySelectorAll('.card'));
-const _pokemonImages = Array.from(document.querySelectorAll('.pokemon-img'))
+const _container = document.querySelector('.container');
 
 async function getData() {
-    const URL = "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0";
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=30&offset=0";
     const response = await fetch(URL);
     const data = await response.json();
 
-    return data.results;
+    return Array.from(data.results);
 }
 
 async function getPokemonData(url) {
@@ -16,18 +15,68 @@ async function getPokemonData(url) {
     return data;
 }
 
-async function createCards() {
-    const generalData = await getData();
+function createCard() {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = `
+        <span class="pokemon-id"></span>
 
-    _cards.forEach(async (_card, index) => {
-        const pokemonData = await getPokemonData(generalData[index].url);
-        const {sprites} = pokemonData;
+        <div class="pokemon-img">
+            <img src="" alt="Pokémon Image">
+        </div>
 
-        _pokemonImages[index].children[0].setAttribute('src', sprites.other["official-artwork"]["front_default"])
-    })
+        <span class="pokemon-name"></span>
 
-    // console.log(generalData);
-    // _pokemonImages.setAttribute('src', sprites.other["official-artwork"]["front_default"]);
+        <div class="pokemon-stats">
+            <div class="weight">
+                <p>Weight</p>
+                <span></span>
+            </div>
+
+            <div class="height">
+                <p>Height</p>
+                <span></span>
+            </div>
+        </div>
+        <p>Type: <span class="type"></span></p>
+    `;
+
+    return card;
 }
 
-createCards();
+async function fillCardsData() {
+    const pokemonsData = await getData();
+    console.log(pokemonsData);
+
+    pokemonsData.forEach(async (pokemon, index) => {
+        const _card = createCard();
+        const _id = _card.querySelector('.pokemon-id');
+        const _image = _card.querySelector('.pokemon-img').children[0];
+        const _name = _card.querySelector('.pokemon-name');
+        const _weight = _card.querySelector('.weight').children[1];
+        const _height = _card.querySelector('.height').children[1];
+        const _type = _card.querySelector('.type');
+       
+        const {id, name, weight, height, types, sprites} = await getPokemonData(pokemonsData[index].url);
+
+
+        _id.textContent = `#${id}`
+        _image.setAttribute('src', sprites.other["official-artwork"]["front_default"]);
+        _name.textContent = name;
+        _weight.textContent = `${weight}kg`;
+        _height.textContent = `${height}m`;
+
+        
+        _container.appendChild(_card);
+        
+    })
+
+    // _cards.forEach(async (_card, index) => {
+    //     const pokemonData = await getPokemonData(generalData[index].url);
+    //     const {sprites} = pokemonData;
+
+    //     _pokemonImages[index].children[0].setAttribute('src', sprites.other["official-artwork"]["front_default"])
+    // })
+}
+
+fillCardsData();
