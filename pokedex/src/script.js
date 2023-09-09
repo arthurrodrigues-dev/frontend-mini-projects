fcd18787cfc86f9e2c0254ce2c9af44657ad9f84
arthur <arthurrodrigues.contato@outlook.com>
2023-09-09 18:00:02 -0300

    refactoring code

--- pokedex/src/script.js
+++ pokedex/src/script.js
@@ -8,7 +8,36 @@ async function getData() {
     return Array.from(data.results);
 }
 
-async function createCards() {
+function createCards(pokemon) {
+    const card = document.createElement('div');
+    card.classList.add('card')
+    card.innerHTML = `
+        <span class="pokemon-id">#${pokemon.id}</span>
+
+        <div class="pokemon-img">
+            <img src="${pokemon.sprites.other["official-artwork"]["front_default"]}" alt="Pokémon Image">
+        </div>
+
+        <span class="pokemon-name">${pokemon.name}</span>
+
+        <div class="pokemon-stats">
+            <div class="weight">
+                <p>Weight</p>
+                <span>${pokemon.weight / 10}kg</span>
+            </div>
+
+            <div class="height">
+                <p>Height</p>
+                <span>${pokemon.height / 10}m</span>
+            </div>
+        </div>
+        <p>Type: <span class="type">${pokemon.types.length > 1? pokemon.types[0].type.name + "/" + pokemon.types[1].type.name : pokemon.types[0].type.name}</span></p>
+    `;
+
+    _container.appendChild(card);
+}
+
+async function renderCards() {
     const pokemonsData = await getData();
     const urls = pokemonsData.map((data) => data.url)
 
@@ -16,32 +45,8 @@ async function createCards() {
     const pokemons = await Promise.all(responses.map((response) => response.json()))
 
     pokemons.forEach((pokemon) => {
-        const card = document.createElement('div');
-        card.classList.add('card')
-        card.innerHTML = `
-            <span class="pokemon-id">#${pokemon.id}</span>
-
-            <div class="pokemon-img">
-                <img src="${pokemon.sprites.other["official-artwork"]["front_default"]}" alt="Pokémon Image">
-            </div>
-
-            <span class="pokemon-name">${pokemon.name}</span>
-
-            <div class="pokemon-stats">
-                <div class="weight">
-                    <p>Weight</p>
-                    <span>${pokemon.weight / 10}kg</span>
-                </div>
-
-                <div class="height">
-                    <p>Height</p>
-                    <span>${pokemon.height / 10}m</span>
-                </div>
-            </div>
-            <p>Type: <span class="type">${pokemon.types.length > 1? pokemon.types[0].type.name + "/" + pokemon.types[1].type.name : pokemon.types[0].type.name}</span></p>
-        `;
-
-        _container.appendChild(card);
+       createCards(pokemon);
     })
 }
-createCards();
+
+renderCards();
